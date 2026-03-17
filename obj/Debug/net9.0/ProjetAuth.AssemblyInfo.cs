731dd9a7bbb6b2d4b0a602c0b952681c3753ad4e
Jerry107 <jerrychico42@gmail.com>
2026-03-17 14:13:03 -0300

    configuration et implementation de authentification des utilisateur

--- obj/Debug/net9.0/ProjetAuth.AssemblyInfo.cs
+++ obj/Debug/net9.0/ProjetAuth.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetAuth")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f191d9a3f5efbce9f6e6a2d6e53862bb4a58a4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bea687db9772a4150329ec91efbb7cbc26f9f24c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetAuth")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetAuth")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
